9baa7e138227ab123e463cd66019a18c5926ff80
yz1201 <15957121194@163.com>
2020-07-23 22:16:24 +0800

    nothing, 福利脑图，有缘者得之。

--- jdbcDemo/src/test/java/cn/dbdj1201/demo/service/impl/UserServiceTest.java
+++ jdbcDemo/src/test/java/cn/dbdj1201/demo/service/impl/UserServiceTest.java
@@ -3,11 +3,15 @@ package cn.dbdj1201.demo.service.impl;
 import cn.dbdj1201.demo.DemoApplication12010;
 import cn.dbdj1201.demo.mapper.UserModelMapper;
 import cn.dbdj1201.demo.service.IUserService;
+import cn.hutool.db.nosql.redis.RedisDS;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import redis.clients.jedis.Jedis;
+
+import java.util.Map;
 
 import static org.junit.Assert.*;
 
@@ -27,5 +31,16 @@ public class UserServiceTest {
 //        boolean login = userService.login("root", "root");
 //        System.out.println(login);
         userService.findAll().forEach(System.out::println);
+    }
+
+    @Test
+    public void test1() {
+        Jedis jedis = RedisDS.create().getJedis();
+//        Jedis jedis = new Jedis();
+//        String set = jedis.set("124", "2323");
+
+        Map<String, String> userInfo = jedis.hgetAll("userInfo");
+        userInfo.forEach((key, value) -> System.out.println(key + "=" + value));
+//        System.out.println(set);
     }
 }
